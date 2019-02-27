9dbee14fc7246a5f782058a27ec19ee1cb05c403
demandre <joris.demandre@gmail.com>
2019-02-27 17:19:41 +0100

    use cookie session and body-parser in app.js, and log all requests headers, body and session

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 // Libs
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var mysql = require('mysql');
-var port = process.env.PORT || '3000';
-// Express
+var bodyParser = require('body-parser');
 var app = express();
-// declaring routes
-var routes = require('./routes/');
-app.use(routes);
+// Vars
+var port = process.env.PORT || '3000';
+
 // Views setting
 app.set('views', './views');
 app.set('view engine', 'pug');
+
 // Use port given
 app.set('port', port);
-// Use cookie parser
-app.use(cookieParser());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 // Define static files path
 app.use(express.static(path.join(__dirname, 'public')));
+
 //Database connection
-app.use(function(req, res, next){
+app.use(function mysqlConnect (req, res, next){
   res.locals.connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'joris',
-    password : 'joris',
-    database : 'node_blog'
+    'host'     : 'localhost',
+    'user'     : 'joris',
+    'password' : 'joris',
+    'database' : 'node_blog'
   });
   //res.locals.connect();
   next();
 });
 
-app.listen(port,function () {
+// Cookies/Session config
+app.use(cookieSession({
+  'name': 'session',
+  'keys': ['key1', 'key2'],
+  'maxAge': 24 * 60 * 60 * 1000
+}));
+
+// Log all requests
+app.use(function logRequest (req, res, next) {
+  console.log(req.headers);
+  console.log(req.body);
+  console.log(req.session);
+  next()
+});
+
+// declaring routes
+var routes = require('./routes/');
+
+app.use(routes);
+
+app.listen(port,function listen () {
   console.log('Server listening on port ' + 3000);
 });
 
