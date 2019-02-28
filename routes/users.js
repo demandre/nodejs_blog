eb324dc56c565ce1ffad01f32b591a20e706bbce
demandre <joris.demandre@gmail.com>
2019-02-28 16:41:01 +0100

    add post create user

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,29 @@ router.get('/users', function(req, res, next) {
   }
 });
 
+/* Post users create */
+router.post('/users/create', function(req, res, next) {
+  if(req.session.is_admin) {
+    var createUserQuery = "insert into user (name,first_name,mail,password_hash,avatar_img_path,is_admin) values (" +
+        "'" + req.body.name + "'," +
+        "'" + req.body.first_name + "'," +
+        "'" + req.body.mail + "'," +
+        "SHA2('" + req.body.password + "',256)," +
+        "'" + req.body.avatar_img_path + "'," +
+        "" + req.body.is_admin + "" +
+        ");";
+
+    res.locals.connection.query(createUserQuery, function (error, results, fields) {
+      if(error != null) {
+        console.log(error);
+        res.render('user', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+      } else {
+        res.render('user', {'message': 'User has been created!', 'is_admin': req.session.is_admin});
+      }
+    });
+  } else {
+    res.redirect('/user');
+  }
+});
+
 module.exports = router;
