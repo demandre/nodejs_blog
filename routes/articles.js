42d9179f510059ca9e0a3fa4dcc00fc6ca52f027
demandre <joris.demandre@gmail.com>
2019-02-28 21:28:40 +0100

    add modify article view

--- routes/articles.js
+++ routes/articles.js
@@ -50,4 +50,30 @@ router.get('/articles/see', function(req, res, next) {
   }
 });
 
+/* Get articles modify */
+router.get('/articles/modify', function(req, res, next) {
+  if(req.session.is_admin) {
+    if(req.urlParams.id) {
+      var selectArticleQuery = 'SELECT * from article ' +
+        "where article_id=" + req.urlParams.id + ";";
+
+      res.locals.connection.query(selectArticleQuery, function (error, articleResults, fields) {
+        if(error != null) {
+          console.log('error');
+          res.render('articleModify', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+        }
+        if(articleResults.length > 0) {
+            res.render('articleModify', {'article': articleResults[0], 'is_admin': req.session.is_admin});
+        } else {
+          res.render('articleModify', {'message': 'This article does not exists', 'is_admin': req.session.is_admin});
+        }
+      });
+    } else {
+      res.redirect('/articles', {'is_admin': req.session.is_admin});
+    }
+  } else {
+    res.redirect('/user');
+  }
+});
+
 module.exports = router;
