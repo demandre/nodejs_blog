4492d85d7a5d1a512818561f293846ab6cdea1c8
demandre <joris.demandre@gmail.com>
2019-02-28 15:49:10 +0100

    add user account page

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,20 @@
 var router = require('express').Router();
 
-/* GET users listing */
+/* Get user info */
 router.get('/user', function(req, res, next) {
-  res.render('user', {'is_admin': req.session.is_admin});
+  var selectUserQuery = 'SELECT name,first_name,mail,avatar_img_path from user ' +
+      "where user_id=" + req.session.user_id + ";";
+
+  res.locals.connection.query(selectUserQuery, function (error, results, fields) {
+    if(error != null) {
+      res.render('user', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+    }
+    if(results.length > 0) {
+      res.render('user', {'user': results[0], 'is_admin': req.session.is_admin});
+    } else {
+      res.render('user', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+    }
+  });
 });
 
 module.exports = router;
