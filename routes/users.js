960422bc78395cc1858cee54541edf5bb24ac7ab
demandre <joris.demandre@gmail.com>
2019-02-28 17:51:25 +0100

    add user search post and adapted template

--- routes/users.js
+++ routes/users.js
@@ -24,14 +24,41 @@ router.post('/users/create', function(req, res, next) {
     res.locals.connection.query(createUserQuery, function (error, results, fields) {
       if(error != null) {
         console.log(error);
-        res.render('user', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+        res.render('users', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
       } else {
-        res.render('user', {'message': 'User has been created!', 'is_admin': req.session.is_admin});
+        res.render('users', {'message': 'User has been created!', 'is_admin': req.session.is_admin});
       }
     });
   } else {
     res.redirect('/user');
   }
+});
+
+/* Post users search */
+router.post('/users/search', function(req, res, next) {
+    if(req.session.is_admin) {
+        var searchUsersQuery = "select user_id,name,first_name,mail,avatar_img_path,count(comment.author_id) as comment_count from user " +
+            "left join comment on user.user_id = comment.author_id " +
+            "where user.name like '%"+ req.body.search_terms +"%' " +
+            "or user.first_name like '%"+ req.body.search_terms +"%' " +
+            "or user.mail like '%"+ req.body.search_terms +"%' " +
+            "group by user.user_id;";
+
+        res.locals.connection.query(searchUsersQuery, function (error, results, fields) {
+            if(error != null) {
+                console.log(error);
+                res.render('users', {'users_message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+            } else {
+                if(results.length > 0) {
+                    res.render('users', {'users': results, 'is_admin': req.session.is_admin});
+                } else {
+                    res.render('users', {'users_message': 'No users contains those terms', 'is_admin': req.session.is_admin});
+                }
+            }
+        });
+    } else {
+        res.redirect('/user');
+    }
 });
 
 module.exports = router;
