ed845c01b15e1cec1f4c440e0a9afe1ddb6f7416
demandre <joris.demandre@gmail.com>
2019-02-28 18:50:27 +0100

    add publish article function

--- routes/publish.js
+++ routes/publish.js
@@ -9,4 +9,28 @@ router.get('/publish', function(req, res, next) {
   }
 });
 
+/* Post publish */
+router.post('/publish', function(req, res, next) {
+    if(req.session.is_admin) {
+        var createArticleQuery = "insert into article (title,content,author_id,date,cover_img_path) values (" +
+            "'" + req.body.title + "'," +
+            "'" + req.body.content + "'," +
+            "'" + req.session.user_id + "'," +
+            "NOW()," +
+            "'" + req.body.cover_img_path + "'" +
+            ");";
+
+        res.locals.connection.query(createArticleQuery, function (error, results, fields) {
+            if(error != null) {
+                console.log(error);
+                res.render('publish', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+            } else {
+                res.render('publish', {'message': 'Article has been created!', 'is_admin': req.session.is_admin});
+            }
+        });
+    } else {
+        res.redirect('/user');
+    }
+});
+
 module.exports = router;
