189ebf2f14a4a979f2a2aefd61ce33c04bda02c4
demandre <joris.demandre@gmail.com>
2019-02-28 20:18:15 +0100

    add article see logic

--- routes/articles.js
+++ routes/articles.js
@@ -18,4 +18,26 @@ router.get('/articles', function(req, res, next) {
   });
 });
 
+
+/* Get articles see */
+router.get('/articles/see', function(req, res, next) {
+  if(req.urlParams.id) {
+    var selectArticleQuery = 'SELECT * from article ' +
+      "where article_id=" + req.urlParams.id + ";";
+
+    res.locals.connection.query(selectArticleQuery, function (error, results, fields) {
+      if(error != null) {
+        res.render('articles', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+      }
+      if(results.length > 0) {
+        res.render('article', {'article': results[0], 'is_admin': req.session.is_admin});
+      } else {
+        res.render('articles', {'message': 'This article does not exists', 'is_admin': req.session.is_admin});
+      }
+    });
+  } else {
+    res.redirect('/articles', {'is_admin': req.session.is_admin});
+  }
+});
+
 module.exports = router;
