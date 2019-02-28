e6dfe1f8404666f8f2febf0906557e11e5280dd5
demandre <joris.demandre@gmail.com>
2019-02-28 18:20:23 +0100

    add user modify and delete functions

--- routes/users.js
+++ routes/users.js
@@ -61,4 +61,74 @@ router.post('/users/search', function(req, res, next) {
     }
 });
 
+/* Get users modify */
+router.get('/users/modify', function(req, res, next) {
+    if(req.session.is_admin) {
+        if(req.urlParams.id) {
+            var selectUserQuery = 'SELECT user_id,name,first_name,mail,avatar_img_path from user ' +
+                "where user_id=" + req.urlParams.id + ";";
+
+            res.locals.connection.query(selectUserQuery, function (error, results, fields) {
+                if(error != null) {
+                    res.render('users', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+                }
+                if(results.length > 0) {
+                    res.render('userModify', {'user': results[0], 'is_admin': req.session.is_admin});
+                } else {
+                    res.render('users', {'message': 'This user does not exists', 'is_admin': req.session.is_admin});
+                }
+            });        } else {
+            res.redirect('/users');
+        }
+    } else {
+        res.redirect('/user');
+    }
+});
+
+/* Post users modify */
+router.post('/users/modify', function(req, res, next) {
+    if(req.session.is_admin) {
+        if(req.urlParams.id) {
+            var updateUserQuery = "update user " +
+                "set name ='" + req.body.name + "'," +
+                "first_name ='" + req.body.first_name + "'," +
+                "mail = '" + req.body.mail + "'," +
+                "avatar_img_path = '" + req.body.avatar_img_path + "'" +
+                "where user_id=" + req.urlParams.id + ";";
+
+            res.locals.connection.query(updateUserQuery, function (error, results, fields) {
+                if(error != null) {
+                    res.render('users', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+                } else {
+                    res.render('users', {'message': 'User has been updated!', 'is_admin': req.session.is_admin});
+                }
+            });
+        } else {
+            res.redirect('/users');
+        }
+    } else {
+        res.redirect('/user');
+    }
+});
+
+/* Get users delete */
+router.get('/users/delete', function(req, res, next) {
+    if(req.session.is_admin) {
+        if(req.urlParams.id) {
+            var deleteUserQuery = 'DELETE from user ' +
+                "where user_id=" + req.urlParams.id + ";";
+
+            res.locals.connection.query(deleteUserQuery, function (error, results, fields) {
+                if(error != null) {
+                    res.render('users', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+                } else {
+                    res.render('users', {'message': 'User has been deleted', 'is_admin': req.session.is_admin});
+                }
+            });        } else {
+            res.redirect('/users');
+        }
+    } else {
+        res.redirect('/user');
+    }
+});
 module.exports = router;
