3284deac69c1a5bd71684d1d67789a5f24f468e7
demandre <joris.demandre@gmail.com>
2019-02-28 20:17:49 +0100

    add articles list template and logic

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,20 @@ var router = require('express').Router();
 
 /* Get articles */
 router.get('/articles', function(req, res, next) {
-  res.render('articles', {'is_admin': req.session.is_admin});
+  var getArticlesQuery = "Select article_id,title,cover_img_path from article;";
+
+  res.locals.connection.query(getArticlesQuery, function (error, results, fields) {
+    if(error != null) {
+      console.log(error);
+      res.render('articles', {'message': 'An error happened... Try again later!', 'is_admin': req.session.is_admin});
+    } else {
+      if(results.length > 0) {
+        res.render('articles', {'articles': results, 'is_admin': req.session.is_admin});
+      } else {
+        res.render('articles', {'message': 'There is no article!', 'is_admin': req.session.is_admin});
+      }
+    }
+  });
 });
 
 module.exports = router;
