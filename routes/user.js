36352ec46f33db9668acc27bfe785854b63f0fe6
demandre <joris.demandre@gmail.com>
2019-02-28 15:11:37 +0100

    add admin check to show admin sections in header

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 
 /* GET users listing */
 router.get('/user', function(req, res, next) {
-  res.render('user');
+  res.render('user', {'is_admin': req.session.is_admin});
 });
 
 module.exports = router;
